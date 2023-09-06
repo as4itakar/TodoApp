54047ccb4fe590a2dfc0f2ae1c509a2fe83616d4
Obukhov Vsevolod <sevaobuxov61@gmail.com>
2023-09-06 17:16:28 +0300

    add json server

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoComponent } from './todo.component';
 import { TaskComponent } from 'src/app/components/task/task.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [TodoComponent],
   declarations: 
