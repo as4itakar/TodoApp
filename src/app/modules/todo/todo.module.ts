6dfc8dd6d49e74169ba709ecc63244612e82d19a
Obukhov Vsevolod <sevaobuxov61@gmail.com>
2023-09-06 16:44:59 +0300

    add task comp

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoComponent } from './todo.component';
+import { TaskComponent } from 'src/app/components/task/task.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [TodoComponent],
-  declarations: [TodoComponent]
+  declarations: 
+  [
+    TodoComponent,
+    TaskComponent
+  ]
 })
 export class TodoModule { }
